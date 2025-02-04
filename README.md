81c5d801c83e4a4a52e84687dee4ba686126fc01
Diego Lara <diegolara93345@gmail.com>
2025-02-03 22:59:30 -0600

    v

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Trying to make something similar to https://github.com/leonmavr/retrocube/tree/master in Go
+Trying to make something similar to https://github.com/leonmavr/retrocube/tree/master in Go except I will use a rasterizer not a raytracer and use bresenhams line drawing algorithm
+Also ansi escape codes for color
--- cmd/vec.go
+++ cmd/vec.go
@@ -1,21 +1,25 @@
 package cmd
 
+/*
+*
+*  Making my vector library bc why not, matrices later although the C version of this only used vectors I think?
+*  Prob will need them for perspective projection translations.
+*
+ */
 type Vec3 [3]float64
 
 type Vec2 [2]float64
 
 type Vec4 [4]float64
 
-func newVec3(x, y, z float64) Vec3 {
-	return Vec3{x, y, z}
-}
+/*
+*
+* All methods for Vec2
+*
+ */
 
 func newVec2(x, y float64) Vec2 {
 	return Vec2{x, y}
-}
-
-func newVec4(x, y, z, j float64) Vec4 {
-	return Vec4{x, y, z, j}
 }
 
 func (v Vec2) add(v2 Vec2) Vec2 {
@@ -25,12 +29,69 @@ func (v Vec2) add(v2 Vec2) Vec2 {
 	}
 }
 
+func (v Vec2) sub(v2 Vec2) Vec2 {
+	return Vec2{
+		v[0] - v2[0],
+		v[1] - v2[1],
+	}
+}
+
+func (v Vec2) mult(v2 Vec2) Vec2 {
+	return Vec2{
+		v[0] * v2[0],
+		v[1] * v2[1],
+	}
+}
+
+/*
+*
+* All methods for Vec3
+*
+ */
+
+func newVec3(x, y, z float64) Vec3 {
+	return Vec3{x, y, z}
+}
+
 func (v Vec3) add(v2 Vec3) Vec3 {
 	return Vec3{
 		v[0] + v2[0],
 		v[1] + v2[1],
 		v[2] + v2[2],
 	}
+}
+
+func (v Vec3) sub(v2 Vec3) Vec3 {
+	return Vec3{
+		v[0] - v2[0],
+		v[1] - v2[1],
+		v[2] - v2[2],
+	}
+}
+
+func (v Vec3) mult(v2 Vec3) Vec3 {
+	return Vec3{
+		v[0] * v2[0],
+		v[1] * v2[1],
+		v[2] * v2[2],
+	}
+}
+
+func addVec3(v1, v2 Vec3) Vec3 {
+	return Vec3{
+		v1[0] + v2[0],
+		v1[1] + v2[1],
+		v1[2] + v2[2],
+	}
+}
+
+/*
+*
+*  All methods for Vec4
+ */
+
+func newVec4(x, y, z, j float64) Vec4 {
+	return Vec4{x, y, z, j}
 }
 
 func (v Vec4) add(v2 Vec4) Vec4 {
@@ -48,5 +109,23 @@ func (v Vec4) sub(v2 Vec4) Vec4 {
 		v[1] - v2[1],
 		v[2] - v2[2],
 		v[3] - v2[3],
+	}
+}
+
+func (v Vec4) mult(v2 Vec4) Vec4 {
+	return Vec4{
+		v[0] * v2[0],
+		v[1] * v2[1],
+		v[2] * v2[2],
+		v[3] * v2[3],
+	}
+}
+
+func addVec4(v1, v2 Vec4) Vec4 {
+	return Vec4{
+		v1[0] * v2[0],
+		v1[1] * v2[1],
+		v1[2] * v2[2],
+		v1[3] * v2[3],
 	}
 }
