575ee7fbcc409e1c3c547e0ae7183c26bf1bf648
Diego Lara <diegolara93345@gmail.com>
2025-02-04 19:29:05 -0600

    implemented bresenhams line algorithm for 2d

--- cmd/mat.go
+++ cmd/mat.go
@@ -3,6 +3,60 @@ package cmd
 type Mat3 [3 * 3]float64
 type Mat4 [4 * 4]float64
 
+/*
+
+All methods for Mat4
+
+*/
+
 func NewMat4() Mat4 {
 	return Mat4{}
+}
+
+func (m Mat4) Mul(m2 Mat4) Mat4 {
+	var newMat Mat4
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			sum := 0.0
+			for k := 0; k < 4; k++ {
+				sum += m[i*4+k] * m2[k*4+j]
+			}
+			newMat[i*4+j] = sum
+		}
+	}
+	return newMat
+}
+
+func (m Mat4) Add(m2 Mat4) Mat4 {
+	var newMat Mat4
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			newMat[i*4+j] = m[i*4+j] + m2[i*4+j]
+		}
+	}
+	return newMat
+}
+func (m Mat4) Sub(m2 Mat4) Mat4 {
+	var newMat Mat4
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 4; j++ {
+			newMat[i*4+j] = m[i*4+j] - m2[i*4+j]
+		}
+	}
+	return newMat
+}
+
+func AddMat4(a, b Mat4) Mat4 {
+	newMat := a.Add(b)
+	return newMat
+}
+
+func SubMat4(a, b Mat4) Mat4 {
+	newMat := a.Sub(b)
+	return newMat
+}
+
+func MulMat4(a, b Mat4) Mat4 {
+	newMat := a.Mul(b)
+	return newMat
 }
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"math"
 
 	"github.com/gdamore/tcell/v2"
 )
@@ -23,45 +23,48 @@ func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string
 	}
 }
 
-func drawBox(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string) {
-	if y2 < y1 {
-		y1, y2 = y2, y1
+// Bresenhams line algorithm implemented from: https://zingl.github.io/bresenham.html
+func drawLine(s tcell.Screen, x0, y0, x1, y1 int, style tcell.Style) {
+	dx := int(math.Abs(float64(x1 - x0)))
+	dy := -int(math.Abs(float64(y1 - y0)))
+	sx := 0
+	sy := 0
+	err := dx + dy
+	if x0 < x1 {
+		sx = 1
+	} else {
+		sx = -1
 	}
-	if x2 < x1 {
-		x1, x2 = x2, x1
+	if y0 < y1 {
+		sy = 1
+	} else {
+		sy = -1
 	}
-
-	// Fill background
-	for row := y1; row <= y2; row++ {
-		for col := x1; col <= x2; col++ {
-			s.SetContent(col, row, ' ', nil, style)
+	for {
+		s.SetContent(x0, y0, '@', nil, style)
+		e2 := 2 * err
+		if e2 >= dy {
+			if x0 == x1 {
+				break
+			} else {
+				err += dy
+				x0 += sx
+			}
+		}
+		if e2 <= dx {
+			if x0 == x1 {
+				break
+			} else {
+				err += dx
+				y0 += sy
+			}
 		}
 	}
-
-	// Draw borders
-	for col := x1; col <= x2; col++ {
-		s.SetContent(col, y1, tcell.RuneHLine, nil, style)
-		s.SetContent(col, y2, tcell.RuneHLine, nil, style)
-	}
-	for row := y1 + 1; row < y2; row++ {
-		s.SetContent(x1, row, tcell.RuneVLine, nil, style)
-		s.SetContent(x2, row, tcell.RuneVLine, nil, style)
-	}
-
-	// Only draw corners if necessary
-	if y1 != y2 && x1 != x2 {
-		s.SetContent(x1, y1, tcell.RuneULCorner, nil, style)
-		s.SetContent(x2, y1, tcell.RuneURCorner, nil, style)
-		s.SetContent(x1, y2, tcell.RuneLLCorner, nil, style)
-		s.SetContent(x2, y2, tcell.RuneLRCorner, nil, style)
-	}
-
-	drawText(s, x1+1, y1+1, x2-1, y2-1, style, text)
 }
 
 func main() {
 	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
-	boxStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorPurple)
+	//boxStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite).Background(tcell.ColorPurple)
 
 	// Initialize screen
 	s, err := tcell.NewScreen()
@@ -76,14 +79,11 @@ func main() {
 	s.EnablePaste()
 	s.Clear()
 
-	// Draw initial boxes
-	drawBox(s, 1, 1, 42, 7, boxStyle, "Click and drag to draw a box")
-	drawBox(s, 5, 9, 32, 14, boxStyle, "Press C to reset")
+	width, height := s.Size()
 
+	drawLine(s, 0, 0, width-1, height-1, defStyle)
 	quit := func() {
-		// You have to catch panics in a defer, clean up, and
-		// re-raise them - otherwise your application can
-		// die without leaving any diagnostic trace.
+		// panics have to be caught or else program will crash without a trace
 		maybePanic := recover()
 		s.Fini()
 		if maybePanic != nil {
@@ -92,17 +92,7 @@ func main() {
 	}
 	defer quit()
 
-	// Here's how to get the screen size when you need it.
-	// xmax, ymax := s.Size()
-
-	// Here's an example of how to inject a keystroke where it will
-	// be picked up by the next PollEvent call.  Note that the
-	// queue is LIFO, it has a limited length, and PostEvent() can
-	// return an error.
-	// s.PostEvent(tcell.NewEventKey(tcell.KeyRune, rune('a'), 0))
-
 	// Event loop
-	ox, oy := -1, -1
 	for {
 		// Update screen
 		s.Show()
@@ -123,21 +113,7 @@ func main() {
 				s.Clear()
 			}
 		case *tcell.EventMouse:
-			x, y := ev.Position()
 
-			switch ev.Buttons() {
-			case tcell.Button1, tcell.Button2:
-				if ox < 0 {
-					ox, oy = x, y // record location when click started
-				}
-
-			case tcell.ButtonNone:
-				if ox >= 0 {
-					label := fmt.Sprintf("%d,%d to %d,%d", ox, oy, x, y)
-					drawBox(s, ox, oy, x, y, boxStyle, label)
-					ox, oy = -1, -1
-				}
-			}
 		}
 	}
 }
